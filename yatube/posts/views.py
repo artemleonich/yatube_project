613a1f546757521fd67d05a008bd4471db8060f7
Artem Leonov <artemleonov@yahoo.com>
2022-01-19 15:23:07 +0300

    Paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,24 +1,26 @@
-from django.shortcuts import get_object_or_404
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
 
-from .models import Group
-from .models import Post
-
-
-POSTS_LIMIT = 10
+from .models import Group, Post, User
 
 
+@login_required
 def index(request):
-    posts = Post.objects.select_related("author", "group")[:POSTS_LIMIT]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    template = "posts/index.html"
     context = {
-        "posts": posts,
+        "page_obj": page_obj,
     }
-    return render(request, "posts/index.html", context)
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:POSTS_LIMIT]
+    posts = group.posts.all()[:10]
     context = {
         "group": group,
         "posts": posts,
