e61c3770ef653912293a2b21e8d5d3fa2ef90e28
Artem Leonov <artemleonov@yahoo.com>
2022-01-19 21:40:13 +0300

    Paginator2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -20,9 +20,14 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:10]
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    template = "posts/group_list.html"
     context = {
         "group": group,
         "posts": posts,
+        "page_obj": page_obj,
     }
-    return render(request, "posts/group_list.html", context)
+    return render(request, template, context)
