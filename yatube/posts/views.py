c484615603ada749757da09ff7583e6feac2cf59
Artem Leonov <artemleonov@yahoo.com>
2022-01-20 14:43:31 +0300

    Paginator final

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,33 +1,36 @@
-from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 
-from .models import Group, Post, User
+from .models import Group
+from .models import Post
 
 
-@login_required
+POSTS_LIMIT = 10
+
+
 def index(request):
-    post_list = Post.objects.all()
-    paginator = Paginator(post_list, 10)
+    post_list = Post.objects.all().order_by("-pub_date")
+    paginator = Paginator(post_list, POSTS_LIMIT)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    template = "posts/index.html"
+    posts = Post.objects.select_related("author", "group")[:POSTS_LIMIT]
     context = {
+        "posts": posts,
         "page_obj": page_obj,
     }
-    return render(request, template, context)
+    return render(request, "posts/index.html", context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.all()
-    paginator = Paginator(posts, 10)
+    posts = Post.objects.filter(group=group)
+    paginator = Paginator(posts, POSTS_LIMIT)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    template = "posts/group_list.html"
     context = {
         "group": group,
         "posts": posts,
         "page_obj": page_obj,
     }
-    return render(request, template, context)
+    return render(request, "posts/group_list.html", context)
