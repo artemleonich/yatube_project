c17ce3aec3fec4a4a3d66113534fc85b1f5f3527
Artem Leonov <artemleonov@yahoo.com>
2022-01-29 11:38:28 +0300

    after pytest7

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import get_object_or_404, redirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpRequest
-from typing import Dict
 
 
 from .models import Group, Post, User
@@ -13,32 +12,44 @@ from .forms import PostForm
 POSTS_LIMIT = 10
 
 
-def page_obj(context: Dict, page_number: int) -> Dict:
-    paginator = Paginator(context, POSTS_LIMIT)
-    return {
-        "paginator": paginator,
-        "page_number": page_number,
-        "page_obj": paginator.get_page(page_number),
-    }
+def paginate(cursor, records):
+    paginator = Paginator(records, POSTS_LIMIT)
+    return paginator.get_page(cursor)
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    context = page_obj(Post.objects.all(), request)
+    posts = Post.objects.select_related("author", "group")
+    page_number = request.GET.get("page")
+    page_obj = paginate(page_number, posts)
+    context = {
+        "page_obj": page_obj,
+    }
     return render(request, "posts/index.html", context)
 
 
 def group_posts(request: HttpRequest, slug: str) -> HttpResponse:
     group = get_object_or_404(Group, slug=slug)
-    context = {"group": group}
-    context.update(page_obj(group.posts.all(), request))
+    posts = group.posts.all()
+    page_number = request.GET.get("page")
+    page_obj = paginate(page_number, posts)
+    context = {
+        "group": group,
+        "page_obj": page_obj,
+    }
     return render(request, "posts/group_list.html", context)
 
 
 def profile(request: HttpRequest, username: str) -> HttpResponse:
     author = get_object_or_404(User, username=username)
+    post_list = author.posts.all()
     count_user_posts = author.posts.count()
-    context = {"author": author, "count_user_posts": count_user_posts}
-    context.update(page_obj(author.posts.all(), request))
+    page_number = request.GET.get("page")
+    page_obj = paginate(page_number, post_list)
+    context = {
+        "author": author,
+        "count_user_posts": count_user_posts,
+        "page_obj": page_obj,
+    }
     return render(request, "posts/profile.html", context)
 
 
@@ -56,9 +67,9 @@ def post_detail(request: HttpRequest, post_id: int) -> HttpResponse:
 @login_required
 def post_create(request: HttpRequest) -> HttpResponse:
     form = PostForm(request.POST or None)
-    form.instance.author = request.user
     if not form.is_valid():
         return render(request, "posts/create_post.html", {"form": form})
+    form.instance.author = request.user
     form.save()
     return redirect("posts:profile", request.user.username)
 
@@ -74,7 +85,7 @@ def post_edit(request: HttpRequest, post_id: int) -> HttpResponse:
         "form": form,
         "is_edit": True,
     }
-    if form.is_valid():
-        form.save()
-        return redirect("posts:post_detail", post_id=post.id)
-    return render(request, template, context)
+    if not form.is_valid():
+        return render(request, template, context)
+    form.save()
+    return redirect("posts:post_detail", post_id=post.id)
