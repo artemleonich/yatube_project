9a4b82f89bf02b613421d8677a583b3ccb5e3580
Artem Leonov <artemleonov@yahoo.com>
2022-01-30 21:06:46 +0300

    after pytest9

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -10,18 +10,22 @@ from .models import Group, Post, User
 from .forms import PostForm
 
 
-POSTS_LIMIT = 10
+POSTS_LIMIT: int = 10
 
 
-def paginate(page_number: int, records: QuerySet) -> Page:
-    paginator = Paginator(records, POSTS_LIMIT)
+def paginate(
+    page_number: int, records: QuerySet, posts_limit: int = POSTS_LIMIT
+) -> Page:
+    paginator = Paginator(records, posts_limit)
     return paginator.get_page(page_number)
 
 
 def index(request: HttpRequest) -> HttpResponse:
     posts = Post.objects.select_related("author", "group")
+
     page_number = request.GET.get("page")
     page_obj = paginate(page_number, posts)
+
     context = {
         "page_obj": page_obj,
     }
@@ -31,8 +35,10 @@ def index(request: HttpRequest) -> HttpResponse:
 def group_posts(request: HttpRequest, slug: str) -> HttpResponse:
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.select_related("author")
+
     page_number = request.GET.get("page")
     page_obj = paginate(page_number, posts)
+
     context = {
         "group": group,
         "page_obj": page_obj,
@@ -42,7 +48,9 @@ def group_posts(request: HttpRequest, slug: str) -> HttpResponse:
 
 def profile(request: HttpRequest, username: str) -> HttpResponse:
     author = get_object_or_404(User, username=username)
+
     post_list = author.posts.all()
+
     page_number = request.GET.get("page")
     page_obj = paginate(page_number, post_list)
 
@@ -59,7 +67,9 @@ def profile(request: HttpRequest, username: str) -> HttpResponse:
 def post_detail(request: HttpRequest, post_id: int) -> HttpResponse:
     post = get_object_or_404(Post, id=post_id)
     author = post.author
+
     author_posts_count = author.posts.count()
+
     context = {
         "post": post,
         "author_posts_count": author_posts_count,
@@ -70,8 +80,10 @@ def post_detail(request: HttpRequest, post_id: int) -> HttpResponse:
 @login_required
 def post_create(request: HttpRequest) -> HttpResponse:
     form = PostForm(request.POST or None)
+
     if not form.is_valid():
         return render(request, "posts/create_post.html", {"form": form})
+
     form.instance.author = request.user
     form.save()
     return redirect("posts:profile", request.user.username)
@@ -93,5 +105,6 @@ def post_edit(request: HttpRequest, post_id: int) -> HttpResponse:
 
     if not form.is_valid():
         return render(request, template, context)
+
     form.save()
     return redirect("posts:post_detail", post_id=post.id)
